96cd5ef0acbe7c63539421a823d6e49990f79e83
Arnon Rodrigues de Paula <arnonrdp@gmail.com>
2021-09-04 19:31:47 -0300

    Botão de Logout

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,13 +1,24 @@
 <template>
   <Header />
   <h1>Definições</h1>
+  <button @click="logout">SAIR</button>
 </template>
 
 <script>
+import { getAuth, signOut } from "firebase/auth";
+
 import Header from "@/components/TheHeader.vue";
 
 export default {
   name: "Settings",
   components: { Header },
+  methods: {
+    logout() {
+      const auth = getAuth();
+      signOut(auth).then(() => {
+        this.$router.push({name: "Login"});
+      });
+    },
+  },
 };
 </script>
