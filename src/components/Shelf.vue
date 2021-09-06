0b0e3d5a88fbaccec3365532d976fbfd78a52d83
Arnon Rodrigues de Paula <arnonrdp@gmail.com>
2021-09-05 21:53:02 -0300

    Ajustes de Layout

--- src/components/Shelf.vue
+++ src/components/Shelf.vue
@@ -2,7 +2,15 @@
   <section>
     <figure>
       <a href="#"><img src="#" alt="Livro"></a>
-      <figcaption>livro.title</figcaption>
+      <figcaption>TÍTULO</figcaption>
+    </figure>
+    <figure>
+      <a href="#"><img src="#" alt="Livro"></a>
+      <figcaption>TÍTULO</figcaption>
+    </figure>
+    <figure>
+      <a href="#"><img src="#" alt="Livro"></a>
+      <figcaption>TÍTULO</figcaption>
     </figure>
   </section>
 </template>
@@ -19,5 +27,19 @@ section {
 	background-image: url('~@/assets/shelfleft.png'), url('~@/assets/shelfright.png'), url('~@/assets/shelfcenter.png');
 	background-repeat: repeat-y, repeat-y, repeat;
 	background-position: top left, top right, 240px 0;
+  padding: 0 30px 15px 30px;
+	border-radius: 6px;
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-around;
+}
+
+section figure {
+	position: relative;
+	display: flex;
+	align-items: flex-end;
+	margin: 0 30px;
+	height: 143.5px;
+	max-width: 80px;
 }
 </style>
