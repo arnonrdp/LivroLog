d8070ded799281d63c22a77dca837ffb532075a9
Arnon Rodrigues de Paula <arnonrdp@gmail.com>
2021-09-05 21:50:54 -0300

    Adicionado Componentes Base

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="$emit('click')">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: ["text"],
+  emits: ["click"],
+};
+</script>
+
+<style scoped>
+button {
+  margin: 5px 0;
+  padding: 10px;
+  border-radius: 18px;
+  background-color: #dee3e6;
+  border: 0.5px solid transparent;
+  box-shadow: var(--high-shadow);
+}
+
+button:hover {
+  box-shadow: var(--low-shadow);
+}
+button:hover img {
+  transform: scale(0.95);
+}
+</style>
