aea891a434753903f21b08fdbe7764bccfc0ded2
Arnon Rodrigues de Paula <arnonrdp@gmail.com>
2021-09-05 21:53:20 -0300

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,20 +4,16 @@
       <h1>UM LUGAR PRA VOCÊ ORGANIZAR<br />TUDO AQUILO QUE VOCÊ JÁ LEU</h1>
     </header>
     <main>
-      <ul>
-        <li>
-          <a @click="activetab = '1'" :class="activetab === '1' ? 'active' : ''"
-            >Entrar</a
-          >
-        </li>
-        <li>
-          <a @click="activetab = '2'" :class="activetab === '2' ? 'active' : ''"
-            >Registrar</a
-          >
-        </li>
-      </ul>
-
+      <div class="menu">
+        <a @click="activetab = '1'" :class="activetab === '1' ? 'active' : ''">
+          Entrar
+        </a>
+        <a @click="activetab = '2'" :class="activetab === '2' ? 'active' : ''">
+          Registrar
+        </a>
+      </div>
       <form v-if="activetab === '1'" action="#" @submit.prevent="submit">
+        <!-- TODO: UTILIZAR O COMPONENT INPUT -->
         <input
           v-model="email"
           type="email"
@@ -36,6 +32,7 @@
       </form>
 
       <form v-if="activetab === '2'" action="#" @submit.prevent="submit">
+        <!-- TODO: UTILIZAR O COMPONENT INPUT -->
         <input v-model="createName" type="text" placeholder="Nome" />
         <input
           v-model="createEmail"
@@ -54,7 +51,7 @@
       </form>
       <hr />
       <p>Entrar com:</p>
-      <button class="social" @click="googleSignIn">
+      <button @click="googleSignIn">
         <img src="/google.svg" alt="" />
       </button>
     </main>
@@ -64,11 +61,9 @@
 <script>
 import {
   getAuth,
-  setPersistence,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
-  browserSessionPersistence,
   signInWithPopup,
 } from "firebase/auth";
 
@@ -143,7 +138,7 @@ export default {
           this.$router.push("/").catch((error) => {
             console.log(error);
           }),
-          // TODO: REMOVER ALERT E INSERIR MENSAGEM PERSONALIZADA
+            // TODO: REMOVER ALERT E INSERIR MENSAGEM PERSONALIZADA
             alert("Sua conta foi criada com sucesso!\nAgora faça login" + user);
         },
         (error) => {
@@ -159,12 +154,12 @@ export default {
 
 <style scoped>
 .container {
-  height: 100vh;
-  font-family: "SF Pro", sans-serif;
   background-image: url("../assets/bg_login.jpg");
+  background-position: top center;
   background-repeat: no-repeat;
   background-size: cover;
-  background-position: top center;
+  font-family: "SF Pro", sans-serif;
+  height: 100vh;
   text-align: center;
 }
 
@@ -177,49 +172,40 @@ header h1 {
 }
 
 main {
-  width: 20em;
-  margin: auto;
-  padding: 2em 1em;
   background-color: var(--primary-bg);
   border-radius: 6px;
+  margin: auto;
+  padding: 2em 1em;
+  user-select: none;
+  width: 20em;
 }
 
-ul {
-  list-style-type: none;
-  margin: 0%;
-  padding: 0 10px 15px;
+.menu {
+  padding: 0 10px 20px;
   display: flex;
-  justify-content: space-around;
 }
 
-ul li {
+.menu a {
   width: 100%;
-  margin: 0 1vw;
+  margin: 0 15px;
 }
 
-ul li a {
-  display: block;
-  font-size: 80%;
+.menu a {
+  font-size: 0.8rem;
   padding: 8px 0;
   border-radius: 6px;
   border: 0.5px solid transparent;
   box-shadow: var(--high-shadow);
 }
 
-ul li a:hover,
-ul li a.active {
+.menu a:hover,
+.menu a.active {
   background-color: #dee3e6;
   box-shadow: var(--low-shadow);
 }
 
-form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
 form input {
-  width: 20em;
+  width: 80%;
   margin: 5px 0;
   padding: 10px;
   overflow: visible;
@@ -231,8 +217,8 @@ form input {
   box-shadow: var(--low-shadow);
 }
 
-form button {
-  margin: 5px 0 0;
+button {
+  margin: 5px 0;
   padding: 10px;
   border-radius: 6px;
   background-color: #dee3e6;
@@ -240,11 +226,10 @@ form button {
   box-shadow: var(--high-shadow);
 }
 
-form button:hover {
+button:hover {
   box-shadow: var(--low-shadow);
 }
-
-button.social {
-  padding: 10px;
+button:hover img {
+  transform: scale(0.95);
 }
 </style>
