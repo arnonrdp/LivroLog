4eecc39903588285d40ae5c882dc613b60f22fbe
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-04 16:24:16 -0300

    Mensagem de desenvolvimento

--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -1,13 +1,16 @@
 <template>
   <Header />
   <h1>{{ $t("followFriends") }}</h1>
+  <Loading />
+  <p>{{ $t('friends.feature-under-dev') }}</p>
 </template>
 
 <script>
 import Header from "@/components/TheHeader.vue";
+import Loading from "@/components/Loading.vue";
 
 export default {
   name: "Friends",
-  components: { Header },
+  components: { Header, Loading },
 };
 </script>
