008016919b1cc665f8fc16590193f588b1b77455
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-04 08:37:01 -0300

    Configuração de variáveis

--- src/App.vue
+++ src/App.vue
@@ -13,21 +13,10 @@ import Header from "@/components/TheHeader.vue";
 export default {
   name: "App",
   components: { Header },
-  //   created() {
-  //     document.cookie = "SameSite=Lax; Secure";
-  //   },
 };
 </script>
 
 <style>
-:root {
-  --primary-bg: #e6e7ee;
-  --primary-text: #44476a;
-  --link-color: #2c3e50;
-  --high-shadow: 3px 3px 6px #b8b9be, -3px -3px 6px #ffffff;
-  --low-shadow: inset 2px 2px 5px #b8b9be, inset -3px -3px 7px #ffffff;
-}
-
 html {
   font-size: 16px;
 }
@@ -70,7 +59,6 @@ html {
 body {
   margin: 0;
   background-color: var(--primary-bg);
-  /* white-space: pre-line; */
 }
 
 #app {
