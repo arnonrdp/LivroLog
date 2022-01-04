6b107d3e0c5b8ea0d1333c77abc4af4bf1e9b572
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-04 18:11:54 -0300

    `Friends.vue` em construção

--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page>
-    <h1 class="text-h6">{{ $t("followFriends") }}</h1>
+    <h1 class="text-h6">{{ $t("friends.followFriends") }}</h1>
+    <Loading />
+    <p>{{ $t("friends.feature-under-dev") }}</p>
   </q-page>
-  <Loading />
-  <p>{{ $t("friends.feature-under-dev") }}</p>
 </template>
 
 <script>
