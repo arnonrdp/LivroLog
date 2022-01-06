922b1d551d052c46b020bab90f456495635879d0
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-06 01:11:24 -0300

    Configurações globais de notificação

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -2,8 +2,13 @@ import "@quasar/extras/material-icons/material-icons.css";
 import { Notify } from "quasar";
 import "./styles/quasar.scss";
 
-// To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
   plugins: { Notify },
+  config: {
+    notify: {
+      position: "bottom",
+      timeout: 2500,
+      progress: true,
+    },
+  },
 };
