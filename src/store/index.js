5bc2b87b99267dbc09c8386f8a12abd14ebe6847
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-15 16:34:27 -0300

    Adição de criptografia ao `LocalStorage`

--- src/store/index.js
+++ src/store/index.js
@@ -14,12 +14,11 @@ const store = createStore({
   actions: {},
   plugins: [
     createPersistedState({
-      // TODO: Descomentar em produção
-      // storage: {
-      //   getItem: (key) => ls.get(key),
-      //   setItem: (key, value) => ls.set(key, value),
-      //   removeItem: (key) => ls.remove(key),
-      // },
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key),
+      },
     }),
   ],
   modules: { authentication, bookstore, people },
