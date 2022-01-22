016049736acc0942a9d42f372ebe363fdf61f558
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-22 15:46:38 -0300

    `q-table` totalmente refatorada

--- src/views/People.vue
+++ src/views/People.vue
@@ -6,73 +6,89 @@
       </template>
     </q-input>
     <q-table
-      grid
-      card-container-class="column"
       :rows="users"
-      row-key="id"
+      :columns="columns"
       :filter="filter"
-      :rows-per-page-options="[0]"
+      :rows-per-page-options="[10]"
+      row-key="id"
+      :loading="loading"
+      :no-results-label="$t('friends.no-one-found')"
+      flat
+      hide-header
+      separator="none"
+      class="q-mt-md bg-transparent"
     >
-      <!-- TODO: Inserir paginação com 10 itens por página   -->
-      <template v-slot:item="props">
-        <div class="flex flex-center justify-between q-my-sm text-secondary">
-          <router-link :to="{ name: 'user', params: { username: props.row.username } }" class="row">
-            <q-avatar>
-              <q-img v-if="props.row.photoURL" :src="props.row.photoURL" alt="avatar" />
-              <q-icon v-else size="md" name="person" />
-            </q-avatar>
-            <div class="column justify-center items-start q-ml-sm">
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="id" :props="props" auto-width>
+            <router-link :to="props.row.username">
+              <q-avatar>
+                <q-img v-if="props.row.photoURL" :src="props.row.photoURL" alt="avatar" />
+                <q-icon v-else size="md" name="person" />
+              </q-avatar>
+            </router-link>
+          </q-td>
+          <q-td key="displayName" :props="props">
+            <router-link :to="props.row.username">
               <strong>{{ props.row.displayName }}</strong>
-              <span>@{{ props.row.username }}</span>
-            </div>
-          </router-link>
-          <q-chip
-            v-if="this.getMyProfile.uid !== props.row.id"
-            disable
-            class="cursor-pointer non-selectable"
-            color="primary"
-            text-color="white"
-            @click="props.row.following ? unfollow(props.row.username) : follow(props.row.username)"
-          >
-            {{ props.row.following ? $t("friends.following") : $t("friends.follow") }}
-            <q-tooltip anchor="bottom middle" self="center middle" class="bg-black">
-              {{ $t("friends.feature-under-dev") }}
-            </q-tooltip>
-          </q-chip>
-        </div>
+              <span class="block">@{{ props.row.username }}</span>
+            </router-link>
+          </q-td>
+          <q-td key="username" :props="props" auto-width>
+            <q-chip
+              v-if="this.getMyProfile.uid !== props.row.id"
+              disable
+              class="cursor-pointer non-selectable"
+              color="primary"
+              text-color="white"
+              @click="props.row.following ? unfollow(props.row.username) : follow(props.row.username)"
+            >
+              {{ props.row.following ? $t("friends.following") : $t("friends.follow") }}
+              <q-tooltip anchor="bottom middle" self="center middle" class="bg-black">
+                {{ $t("friends.feature-under-dev") }}
+              </q-tooltip>
+            </q-chip>
+          </q-td>
+        </q-tr>
       </template>
-      <template v-slot:no-data>
-        <div class="full-width row flex-center q-gutter-sm">
-          <q-icon size="2em" name="sentiment_dissatisfied" />
-          <span>{{ $t("friends.no-one-found") }}</span>
-        </div>
+      <template v-slot:loading>
+        <q-inner-loading showing color="primary" />
       </template>
     </q-table>
   </q-page>
 </template>
 
 <script>
-import Loading from "@/components/Loading.vue";
 import { mapGetters } from "vuex";
 
 export default {
-  components: { Loading },
   data: () => ({
+    columns: [
+      { name: "id", field: "id" },
+      { name: "displayName", field: "displayName", align: "left" },
+      { name: "username", field: "username" },
+    ],
     filter: "",
     friends: [],
+    loading: false,
     offset: 115,
     users: [],
   }),
   computed: {
     ...mapGetters(["getUsers", "getMyProfile"]),
   },
-  async mounted() {
-    await this.$store.dispatch("queryDBUsers");
-    this.users = this.getUsers;
+  mounted() {
+    this.loadUsers();
   },
   methods: {
     myTweak() {
       return { minHeight: `calc(100vh - ${this.offset}px)` };
+    },
+    async loadUsers() {
+      this.loading = true;
+      await this.$store.dispatch("queryDBUsers");
+      this.users = this.getUsers;
+      this.loading = false;
     },
   },
 };
@@ -81,7 +97,7 @@ export default {
 <style scoped>
 .q-page {
   margin: 0 auto;
-  width: 32rem;
+  max-width: 32rem;
 }
 button {
   height: 1.6rem;
