89e6d45ba45a7e178fec437fee217f6a6ed5fbf1
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-24 09:55:52 -0300

    Criação da página Account

--- src/views/SettingsAccount.vue
+++ src/views/SettingsAccount.vue
@@ -1,99 +1,53 @@
 <template>
-  <div class="text-h6">{{ $t("settings.account-profile") }}</div>
-  <q-form @submit="updateProfile" class="q-gutter-md q-mb-md">
-    <q-input v-model="displayName" :label="$t('book.shelfname')">
-      <template v-slot:prepend>
-        <q-icon name="badge" />
-      </template>
-    </q-input>
-    <q-input
-      v-model="username"
-      :label="$t('sign.username')"
-      debounce="500"
-      :prefix="hostname"
-      :rules="[(val) => usernameValidator(val)]"
-    >
-      <template v-slot:prepend>
-        <q-icon name="link" />
-      </template>
-    </q-input>
-    <q-select v-model="locale" :options="localeOptions" :label="$t('settings.language')" emit-value map-options>
-      <template v-slot:prepend>
-        <q-icon name="translate" />
-      </template>
-    </q-select>
-    <!-- TODO: Permitir que o usuário altere seu e-mail -->
-    <!-- TODO: Permitir que o usuário altere sua senha -->
-    <!-- TODO: Permitir que o usuário adicione uma foto de perfil -->
-    <q-btn :label="$t('settings.update-profile')" type="submit" color="primary" icon="save" :loading="updatindProfile" />
-  </q-form>
-  <q-separator />
-  <q-form @submit="updateAccount" class="q-gutter-md q-mt-md">
-    <q-input v-model="email" :label="$t('sign.mail')" :rules="[(val) => emailValidator(val)]">
+  <div class="text-h6">{{ $t("settings.account") }}</div>
+  <!-- TODO: Permitir que o usuário altere sua senha -->
+  <!-- TODO: Permitir que o usuário adicione uma foto de perfil -->
+  <q-form @submit.prevent="updateAccount" class="q-gutter-md q-mt-md">
+    <q-input v-model="email" :label="$t('sign.mail')" :rules="[(val) => emailValidator(val)]" required>
       <template v-slot:prepend>
         <q-icon name="mail" />
       </template>
     </q-input>
-
-    <q-btn :label="$t('settings.update-account')" type="submit" color="primary" />
+    <q-input v-model="password" type="password" :label="$t('sign.password')" :rules="[(val) => passwordValidator(val)]">
+      <template v-slot:prepend>
+        <q-icon name="lock" />
+      </template>
+    </q-input>
+    <q-btn :label="$t('settings.update-account')" type="submit" color="primary" icon="save" :loading="updating" />
   </q-form>
 </template>
 
 <script>
-import { useI18n } from "vue-i18n";
 import { mapGetters } from "vuex";
 
 export default {
   data: () => ({
     email: "",
-    displayName: "",
-    hostname: "",
-    updatingAccount: false,
-    updatindProfile: false,
-    username: "",
+    password: "",
+    updating: false,
   }),
   computed: {
-    ...mapGetters(["getMyEmail", "getMyDisplayName", "getMyUsername"]),
+    ...mapGetters(["getMyEmail"]),
   },
   mounted() {
     this.email = this.getMyEmail;
-    this.displayName = this.getMyDisplayName;
-    this.hostname = window.location.hostname + "/";
-    this.username = this.getMyUsername;
-  },
-  setup() {
-    const { locale } = useI18n({ useScope: "global" });
-    return {
-      locale,
-      localeOptions: [
-        { value: "en", label: "English" },
-        { value: "ja", label: "日本語" },
-        { value: "pt" && "pt-BR", label: "Português" },
-      ],
-    };
   },
   methods: {
-    usernameValidator(username) {
-      if (username === this.getMyUsername) return true;
-      if (["home", "add", "people", "settings", "login"].includes(username)) return false;
-      if (!/^[a-zA-Z0-9_]{3,20}$/.test(username)) return false;
-      return this.$store.dispatch("checkUsername", username).then((exists) => !exists);
+    emailValidator(email) {
+      if (email === this.email) return true;
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) return false;
+      return this.$store.dispatch("checkEmail", email).then((exists) => !exists);
     },
-    updateProfile() {
-      this.updatindProfile = true;
-      this.$store
-        .dispatch("updateProfile", this.displayName, this.username )
-        .then(() => this.$q.notify({ icon: "check_circle", message: this.$t("settings.profile-updated") }))
-        .catch(() => this.$q.notify({ icon: "error", message: this.$t("settings.profile-updated-error") }))
-        .finally(() => (this.updatindProfile = false));
+    passwordValidator(password) {
+      return password.length >= 6;
     },
     updateAccount() {
-      this.updatingAccount = true;
+      this.updating = true;
       this.$store
-        .dispatch("updateAccount", this.email)
+        .dispatch("updateAccount", this.email, this.password)
         .then(() => this.$q.notify({ icon: "check_circle", message: this.$t("settings.account-updated") }))
         .catch(() => this.$q.notify({ icon: "error", message: this.$t("settings.account-updated-error") }))
-        .finally(() => (this.updatingAccount = false));
+        .finally(() => (this.updating = false));
     },
   },
 };
