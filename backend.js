f03132afcdc0f06b58d422d32c268f888edbc23c
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-28 14:07:05 -0300

    Teste de html2canvas-proxy

---
+++ backend.js
@@ -0,0 +1,9 @@
+// var proxy = require("html2canvas-proxy");
+// var express = require("express");
+// const port = 8000;
+
+// var app = express();
+// app.use("/", proxy());
+// app.listen(port, () => {
+//   console.log(`html2canvas-proxy listening on port ${port}`);
+// });
