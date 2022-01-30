fb9541039b96b242cb55371480e0c5df4e50dd9b
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-30 06:50:20 -0300

    Ajustes no alinhamento da Logo

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -42,7 +42,7 @@ export default {
 
 <style scoped>
 header {
-  align-items: center;
+  align-items: baseline;
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.05), inset 0 -1px 0 rgba(0, 0, 0, 0.1);
   display: flex;
   text-align: left;
