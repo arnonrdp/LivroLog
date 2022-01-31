b78cc7d096323200ee1123cd5adc0da866906bcb
Arnon Rodrigues <arnonrdp@gmail.com>
2022-01-31 08:04:17 -0300

    Meta tags transferidas do `index.html` para `App.vue`

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,21 @@
 
 <script>
 import Header from "@/components/TheHeader.vue";
+import { useMeta } from "quasar";
+import { useI18n } from "vue-i18n";
 
 export default {
   components: { Header },
+  setup() {
+    const { t } = useI18n();
+    useMeta({
+      meta: {
+        description: { name: "description", content: t("meta.description") },
+        keywords: { name: "keywords", content: t("meta.keywords") },
+        ogDescription: { name: "og:description", content: t("meta.description") },
+        twitterDescription: { name: "twitter:description", content: t("meta.description") },
+      },
+    });
+  },
 };
 </script>
