fae17962aeccfc7b1f7388c15193e85ca0f68a29
Arnon Rodrigues <arnonrdp@gmail.com>
2022-02-02 10:27:57 -0300

    Teste de meta tags

--- src/App.vue
+++ src/App.vue
@@ -18,29 +18,35 @@ export default {
   setup() {
     const { t } = useI18n();
     const description = ref("");
+    const keywords = ref("");
+    const ogDescription = ref("");
+    const twitterDescription = ref("");
     useMeta(() => ({
       title: "Livrero",
       meta: {
         // Primary Meta Tags
         description: description.value,
-        keywords: { name: "keywords", content: t("meta.keywords") },
+        keywords: keywords.value,
         author: { name: "author", content: "Arnon Rodrigues" },
         // Open Graph / Facebook
         ogType: { name: "og:type", content: "website" },
         ogTitle: { name: "og:title", content: "Livrero" },
-        ogDescription: { name: "og:description", content: t("meta.description") },
+        ogDescription: ogDescription.value,
         ogImage: { name: "og:image", content: "https://livrero.vercel.app/main.jpg" },
         ogUrl: { name: "og:url", content: "https://livrero.vercel.app/" },
         // Twitter
         twitterCard: { name: "twitter:card", content: "summary_large_image" },
         twitterTitle: { name: "twitter:title", content: "Livrero" },
-        twitterDescription: { name: "twitter:description", content: t("meta.description") },
+        twitterDescription: twitterDescription.value,
         twitterImage: { name: "twitter:image", content: "https://livrero.vercel.app/main.jpg" },
         twitterUrl: { name: "twitter:url", content: "https://livrero.vercel.app/" },
       },
     }));
     function setLanguage() {
       description.value = t("meta.description");
+      keywords.value = { name: "keywords", content: t("meta.keywords") };
+      ogDescription.value = { name: "og:description", content: t("meta.description") };
+      twitterDescription.value = { name: "twitter:description", content: t("meta.description") };
     }
     return {
       setLanguage,
