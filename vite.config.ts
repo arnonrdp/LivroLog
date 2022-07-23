5d91204131e5ae61b5b8e38ee208de0eecb076e5
Arnon <arnonrdp@gmail.com>
2022-07-23 06:01:15 -0300

    Teste de redução do build

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          const chunks = ['ant-design-vue', 'vue-router', 'vue', 'dplayer', 'swiper', 'moment', 'axios', 'socket', 'events', 'lodash', 'qs']
+          const chunks = ['axios', 'firebase', 'fsevents', 'lodash', 'pinia', 'quasar', 'typescript', 'vue', 'vue-i18n', 'vue-router']
           if (id.includes('/node_modules/')) {
             for (const chunkName of chunks) {
               if (id.includes(chunkName)) {
@@ -25,6 +18,12 @@ export default defineConfig({
           }
         }
       }
+    }
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
