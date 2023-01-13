90a99bc23e7e2fae104588e77734b798e495816a
Arnon <arnonrdp@gmail.com>
2023-01-13 18:22:47 -0300

    Painel de idioma adicionado

---
+++ src/components/SettingsLanguage.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-form @submit.prevent="preferedLanguage" class="q-gutter-md q-mb-md">
+    <q-select v-model="locale" :options="localeOptions" :label="$t('settings.language')" emit-value map-options>
+      <template v-slot:prepend>
+        <q-icon name="translate" />
+      </template>
+    </q-select>
+    <div class="text-center">
+      <q-btn :label="$t('settings.save')" type="submit" color="primary" icon="save" :loading="updating" />
+    </div>
+  </q-form>
+</template>
+
+<script setup lang="ts">
+import { localeOptions } from '@/i18n'
+import { useMeta } from 'quasar'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+const updating = ref(false)
+
+useMeta({
+  title: `LivroLog | ${t('settings.language')}`,
+  meta: {
+    ogTitle: { name: 'og:title', content: `LivroLog | ${t('settings.language')}` },
+    twitterTitle: { name: 'twitter:title', content: `LivroLog | ${t('settings.language')}` }
+  }
+})
+
+const { locale } = useI18n({ useScope: 'global' })
+
+function preferedLanguage() {
+  // TODO: develop this function
+  updating.value = true
+  setTimeout(() => {
+    updating.value = false
+  }, 1000)
+}
+</script>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,35 +3,41 @@
     <q-tabs v-model="tab" class="text-teal">
       <q-tab name="readDates" icon="event" :label="$t('settings.books')" />
       <q-tab name="profile" icon="person" :label="$t('settings.profile')" />
+      <q-tab name="language" icon="translate" :label="$t('settings.language')" />
       <q-tab name="account" icon="email" :label="$t('settings.account')" />
       <q-tab name="password" icon="lock" :label="$t('sign.password')" />
     </q-tabs>
 
     <q-tab-panels v-model="tab" animated swipeable transition-prev="jump-up" transition-next="jump-up">
       <q-tab-panel name="readDates">
-        <SettingsBooksView />
+        <SettingsBooks />
       </q-tab-panel>
 
       <q-tab-panel name="profile">
-        <SettingsProfileView />
+        <SettingsProfile />
+      </q-tab-panel>
+
+      <q-tab-panel name="language">
+        <SettingsLanguage />
       </q-tab-panel>
 
       <q-tab-panel name="account">
-        <SettingsAccountView />
+        <SettingsAccount />
       </q-tab-panel>
 
       <q-tab-panel name="password">
-        <SettingsPasswordView />
+        <SettingsPassword />
       </q-tab-panel>
     </q-tab-panels>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import SettingsAccountView from '@/components/SettingsAccount.vue'
-import SettingsBooksView from '@/components/SettingsBooks.vue'
-import SettingsPasswordView from '@/components/SettingsPassword.vue'
-import SettingsProfileView from '@/components/SettingsProfile.vue'
+import SettingsAccount from '@/components/SettingsAccount.vue'
+import SettingsBooks from '@/components/SettingsBooks.vue'
+import SettingsLanguage from '@/components/SettingsLanguage.vue'
+import SettingsPassword from '@/components/SettingsPassword.vue'
+import SettingsProfile from '@/components/SettingsProfile.vue'
 import { ref } from 'vue'
 
 const tab = ref('readDates')
