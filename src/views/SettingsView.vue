aa955e425b0e9ed9f6998f8ac10a32d353d2afaf
Arnon <arnonrdp@gmail.com>
2023-01-13 18:02:20 -0300

    Adicionado paineis de configuração

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,8 +1,41 @@
 <template>
   <q-page padding>
-    <router-view />
+    <q-tabs v-model="tab" class="text-teal">
+      <q-tab name="readDates" icon="event" :label="$t('settings.books')" />
+      <q-tab name="profile" icon="person" :label="$t('settings.profile')" />
+      <q-tab name="account" icon="email" :label="$t('settings.account')" />
+      <q-tab name="password" icon="lock" :label="$t('sign.password')" />
+    </q-tabs>
+
+    <q-tab-panels v-model="tab" animated swipeable transition-prev="jump-up" transition-next="jump-up">
+      <q-tab-panel name="readDates">
+        <SettingsBooksView />
+      </q-tab-panel>
+
+      <q-tab-panel name="profile">
+        <SettingsProfileView />
+      </q-tab-panel>
+
+      <q-tab-panel name="account">
+        <SettingsAccountView />
+      </q-tab-panel>
+
+      <q-tab-panel name="password">
+        <SettingsPasswordView />
+      </q-tab-panel>
+    </q-tab-panels>
   </q-page>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import SettingsAccountView from './SettingsAccountView.vue'
+import SettingsBooksView from './SettingsBooksView.vue'
+import SettingsPasswordView from './SettingsPasswordView.vue'
+import SettingsProfileView from './SettingsProfileView.vue'
+
+const tab = ref('readDates')
+</script>
 
 <style scoped>
 .q-page {
