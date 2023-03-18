4566efa4176a8f0aa1ba34d4ec4840b5a2115f0c
Arnon <arnonrdp@gmail.com>
2023-03-18 09:51:50 -0300

    Tipagens adicionadas e melhoradas

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Book } from '@/models'
 
 export interface User {
-  uid: string
+  books?: Book[]
   displayName: string
   email: string
+  followers?: GoogleUser[]
+  following?: GoogleUser[]
   locale: object | string
   modifiedAt?: Date
   password: string
   photoURL?: string
   shelfName?: string
+  uid: string
   username: string
-  books?: Book[]
-  followers?: User[]
-  following?: User[]
+}
+
+export interface GoogleUser {
+  [field: string]: any
 }
