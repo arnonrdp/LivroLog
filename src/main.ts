4fe34afdac7ed152051ce7311540f248a5309584
Arnon R <arnonrdp@gmail.com>
2024-05-24 05:50:38 -0300

    chore: Update Quasar configuration and dependencies
    This commit updates the Quasar configuration and dependencies. The changes include:
    
    - Importing additional Quasar components and plugins in the main.ts file
    - Removing the quasar-user-options.ts file
    - Updating the Quasar configuration in the main.ts file to include the new components and plugins
    - Adding new CSS styles for Quasar in the main.ts file
    
    These changes were made to improve the functionality and performance of the application.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import { Quasar } from 'quasar'
+import { LocalStorage, Meta, Notify, Quasar } from 'quasar'
+import lang from 'quasar/lang/pt-BR.js'
 import { createApp } from 'vue'
 import { i18n } from './i18n'
-import quasarUserOptions from './quasar-user-options'
+
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
 import router from './router'
@@ -13,7 +16,10 @@ const pinia = createPinia()
 
 pinia.use(piniaPluginPersistedstate)
 
-app.use(Quasar, quasarUserOptions)
+app.use(Quasar, {
+  plugins: { LocalStorage, Notify, Meta },
+  lang: lang
+})
 app.use(router)
 app.use(pinia)
 app.use(i18n)
