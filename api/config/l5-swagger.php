73ae099f5b1500481e14a2af118da0afd1407eb6
Arnon R <arnonrdp@gmail.com>
2025-08-10 17:43:11 -0300

    refactor: Remove L5_SWAGGER_CONST_HOST constant; use explicit servers in AuthController

--- api/config/l5-swagger.php
+++ api/config/l5-swagger.php
@@ -312,7 +312,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('APP_URL', 'http://localhost'),
+            // Removed L5_SWAGGER_CONST_HOST - using explicit servers in AuthController
         ],
     ],
 ];
